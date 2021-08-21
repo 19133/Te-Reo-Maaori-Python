2ab957caa12e1b5aa5ca5333f4be55ea7f71ad56
Kafi Ashkir <82185473+19133@users.noreply.github.com>
2021-08-21 07:48:17 +0000

    added while and break to the difficulty component so if the user does something that falls into the else statement the question repeats

--- difficulty.py
+++ difficulty.py
@@ -1,17 +1,22 @@
-# asks user if they would like to play the quiz on easy, medium, or hard
-user_difficulty = input("What difficulty would you like to play. Easy, normal, or hard\n").lower()
 
-# if easy, the program will print easy difficulty questions (Haven't made the questions and answer component yet)
-if user_difficulty == "easy":
-  print ("program continues")
+while True:
+  # asks user if they would like to play the quiz on easy, medium, or hard
+  user_difficulty = input("Would you like to play the quiz om easy, normal, or hard\n").lower()
 
-# if medium, the program will print medium difficulty questions (Haven't made the questions and answer component yet)
-elif user_difficulty == "medium":
-  print ("program continues")
+  # if easy, the program will print easy difficulty questions (Haven't made the questions and answer component yet)
+  if user_difficulty == "easy":
+    print ("program continues")
+    break
 
-# if hard, the program will print hard difficulty questions (Haven't made the questions and answer component yet)
-elif user_difficulty == "hard":
-  print ("program continues")
+  # if medium, the program will print medium difficulty questions (Haven't made the questions and answer component yet)
+  elif user_difficulty == "medium":
+    print ("program continues")
+    break
 
-else:
-  print("please type easy, medium, or hard")
+  # if hard, the program will print hard difficulty questions (Haven't made the questions and answer component yet)
+  elif user_difficulty == "hard":
+    print ("program continues")
+    break
+
+  else:
+    print("please type easy, medium, or hard")
---
+++ main.py
@@ -0,0 +1,42 @@
+
+#funtions
+
+#Played before function
+def played_before(question):
+  valid=False
+  while not valid:
+    user_response = input (question) .lower()
+
+    if user_response == "yes" or user_response  == "y":   
+        user_response = "yes"
+        return user_response
+      
+    elif user_response == "no" or user_response  == "n":
+        user_response = "no"
+        return user_response 
+
+    else:
+      print("Please say yes or no") 
+
+# welcomes user to the Te Reo Maaori quiz
+print("Welcome to the Te Reo Maaori quiz")
+# asks user thier name
+print("What is your wonderful name")
+name = input()
+#Greets user in Te Reo Maaori
+print("Kia ora" + name)
+
+# asks user if they have played the Te Reo Maaori quiz before
+played_quiz = played_before ("have you played the Te Reo Maaori quiz before" + name).lower()
+
+# if yes, the the program continues
+if played_before == "yes":
+  print("Program continues")
+
+# else if no, then the program explains what it is then continues
+elif played_before == "no":
+  print("The Te Reo Maaori quiz is a program which tests and helps students enhance their Te Reo Maaori language skill")
+
+# else, asks user to type yes or no then the question repeats
+else:
+  print("Please type yes or no")
