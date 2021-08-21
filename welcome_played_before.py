51c7ec8d8ec5359f91f1bcdcba9b7d73b4df5c07
Kafi Ashkir <82185473+19133@users.noreply.github.com>
2021-08-21 07:18:02 +0000

    edited played before component by changing it into a function. I also added return user_response into the function. I did that because it makes the played before question repeat if the user does something that falls into the else statement.

--- welcome_played_before.py
+++ welcome_played_before.py
@@ -1,9 +1,29 @@
+
+#funtions
+
+#Played before function
+def played_before(question):
+  valid=False
+  while not valid:
+    user_response = input (question) .lower()
+
+    if user_response == "yes" or user_response  == "y":   
+        user_response = "yes"
+        return user_response
+      
+    elif user_response == "no" or user_response  == "n":
+        user_response = "no"
+        return user_response 
+
+    else:
+      print("Please say yes or no") 
+
 print("Welcome to the Te Reo Maaori quiz")
 print("What is your wonderful name")
 name = input()
 print("Kia ora" + name)
 
-played_before = input ("have you played the Te Reo Maaori quiz before" + name)
+played_quiz = played_before ("have you played the Te Reo Maaori quiz before" + name)
 
 if played_before == "yes":
   print("Program continues")
